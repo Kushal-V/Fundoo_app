32ac5e545e2fcb2bf723f748344b1373b3ed0910
Kushal <kushalvekariya98@gmail.com>
2026-01-19 15:02:01 +0530

    feat(user): [Kushal] complete user CRUD with FastAPI and Argon2 hashing

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
+from routes import user_route
 from db.base import Base
 from db.session import engine
-from routes import user_route
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Fundoo App Backend")
 
-app.include_router(user_route.router, prefix="/users", tags=["Users"])
+app.include_router(user_route.router)
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,13 +4,13 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
+class UserUpdate(BaseModel):
+    email: EmailStr | None = None
+    password: str | None = None
 
 class UserResponse(BaseModel):
     id: int
     email: EmailStr
 
     class Config:
-        orm_mode = True
+        from_attributes = True
