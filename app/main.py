ca50fc46e69c556fe2ff4edcc60b5415af072a0e
Kushal <kushalvekariya98@gmail.com>
2026-01-19 15:26:54 +0530

    setup: [Kushal] register notes router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from routes import user_route
 from db.base import Base
 from db.session import engine
+from routes import user_route, note_route
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Fundoo App Backend")
 
 app.include_router(user_route.router)
+app.include_router(note_route.router)
